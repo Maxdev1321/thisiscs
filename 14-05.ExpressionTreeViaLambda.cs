01b2d58234dc97ef59d38818254a71dd14306ed9
Maxdev1321 <75409729+Maxdev1321@users.noreply.github.com>
2020-12-07 14:56:13 +0900

    Create 14-05.ExpressionTreeViaLambda.cs

---
+++ 14-05.ExpressionTreeViaLambda.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq.Expressions;
+
+namespace ExpressionTreeViaLambda
+{
+    class MainApp
+    {
+        static void Main(string[]args)
+        {
+            Expression<Func<int, int, int>> expression = (a, b) => 1 * 2 + (a - b);
+            Func<int, int, int> func = expression.Compile();
+
+            Console.WriteLine($"1*2+({7}-{8}) = {func(7, 8)}");
+        }
+    }
+}
