c4c70c9385330a46d4c2ea4aed71dc57a2abdf46
Maxdev1321 <75409729+Maxdev1321@users.noreply.github.com>
2020-12-07 15:21:21 +0900

    Create 14-06.ExpressionBodiedMember.cs

---
+++ 14-06.ExpressionBodiedMember.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+
+namespace ExpressionBodiedMember
+{
+    class FriendList
+    {
+        private List<string> list = new List<string>();
+
+        public void Add(string name) => list.Add(name);
+        public void Remove(string name) => list.Remove(name);
+        public void PrintAll()
+        {
+            foreach (var s in list)
+                Console.WriteLine(s);
+        }
+
+        public FriendList() => Console.WriteLine("FriendList()");
+        ~FriendList() => Console.WriteLine("~FriendList()");
+
+        public int Capacity
+        {
+            get => list.Capacity;
+            set => list.Capacity = value;
+        }
+
+        public string this[int index]
+        {
+            get => list[index];
+            set => list[index] = value;
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[]args)
+        {
+            FriendList obj = new FriendList();
+            obj.Add("Eeny");
+            obj.Add("Meeny");
+            obj.Add("Miny");
+            obj.Remove("Eeny");
+            obj.PrintAll();
+
+            Console.WriteLine($"{obj.Capacity}");
+            obj.Capacity = 10;
+            Console.WriteLine($"{obj[0]}");
+            obj[0] = "Moe";
+            obj.PrintAll();
+        }
+    }
+}
