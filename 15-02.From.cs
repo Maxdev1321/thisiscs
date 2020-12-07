fa7321b6d76b8f61317a9f6e35dcfe1e4a093caa
Maxdev1321 <75409729+Maxdev1321@users.noreply.github.com>
2020-12-07 18:31:45 +0900

    Create 15-02.From.cs

---
+++ 15-02.From.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Linq;
+
+namespace From
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 9, 2, 6, 4, 5, 3, 7, 8, 1, 10 };
+
+            var result = from n in numbers
+                         where n % 2 == 0
+                         orderby n
+                         select n;
+
+            var result2 = from n in numbers
+                          orderby n
+                          select n;
+
+            foreach (int n in result)
+                Console.WriteLine($"짝수 : {n}");
+
+            foreach (int n in result2)
+                Console.WriteLine($"{n}");
+        }
+    }
+}
